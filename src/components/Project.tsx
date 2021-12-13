30ea3a9712c47e07aa430dddc63e67ad9152647e
Ben Shua <47493928+benllshua@users.noreply.github.com>
2021-12-13 21:34:26 +0200

    home

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -55,6 +55,11 @@ const useStyles = makeStyles((theme: Theme) =>
       backdropFilter: 'blur(2px)',
       backgroundColor: '#ffffff33',
     },
+    imgFlexWrapper: {
+      flexGrow: 0,
+      flexShrink: 0,
+      flexBasis: '50%',
+    },
   })
 );
 
@@ -85,13 +90,15 @@ const Project = ({ project }: Props) => {
     <Grid container className={classes.root}>
       <Grid item md={6}>
         <ImageListItem className={classes.image}>
-          <Image
-            layout="fill"
-            src={mainImg}
-            alt={`${title} project image`}
-            placeholder={'blur'}
-            blurDataURL={mainImg}
-          />
+          <div className={classes.imgFlexWrapper}>
+            <Image
+              layout="fill"
+              src={mainImg}
+              alt={`${title} project image`}
+              placeholder={'blur'}
+              blurDataURL={mainImg}
+            />
+          </div>
           <ImageListItemBar
             sx={{
               background:
