9f6b4143244f92b66d019283f1811abdd83b73de
Benllshua <benshua2@gmail.com>
2023-05-30 19:51:58 +0300

    lazy loading, will it make my app faster?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@
 
 import Theme from '@/theme';
 import { Container, CssBaseline, NoSsr, useMediaQuery, useTheme } from '@mui/material';
+import dynamic from 'next/dynamic';
 import Footer from '../components/Footer';
-import ParticalsBackground from '../components/ParticalsBackground';
 import SocialMediaLinks from '../components/SocialMediaLinks';
 import ThemeController from '../components/ThemeController';
 import About from '../components/sections/About';
 import Landing from '../components/sections/Landing';
-import Portfolio from '../components/sections/Portfolio';
-import Services from '../components/sections/Services';
-import Skills from '../components/sections/Skills';
 import { useParticlesStore } from '../context/useParticlesMode';
+
+const Portfolio = dynamic(() => import('../components/sections/Portfolio'));
+const Services = dynamic(() => import('../components/sections/Services'));
+const Skills = dynamic(() => import('../components/sections/Skills'));
+const ParticalsBackground = dynamic(() => import('../components/ParticalsBackground'));
 
 export default function Page() {
   const particalsMode = useParticlesStore((state) => state.mode);
